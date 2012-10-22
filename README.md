939fe0c8e71a392b17336a9dd9c1dc6e761b0aca
Jonathan Chang <jchang641@gmail.com>
2012-10-21 18:39:01 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Dotfiles
+
+Some stuff cribbed from:
+* https://github.com/lloeki/dotfiles
---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+[ $(pwd) = "$HOME/.dotfiles" ] || ln -sfn "$(pwd)" "$HOME/.dotfiles"
+
+for file in home/*; do
+dotfile="$(basename "$file")"
+    case "${dotfile}" in
+        Makefile|*.md|LICENSE|setup.sh)
+            # NOOP
+            ;;
+        *)
+            # link the file with a leading dot
+            echo "linking ${dotfile}"
+            ln -sf "$(pwd)/${file}" "$HOME/.${dotfile}"
+            ;;
+    esac
+done
