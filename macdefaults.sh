e42d5cf720477936a0c16cab3d74087dc8fd319c
Jonathan Chang <jchang641@gmail.com>
2017-07-24 13:54:22 -0700

    Fix default repeat rate

--- macdefaults.sh
+++ macdefaults.sh
@@ -11,7 +11,7 @@ printf "Keyboard - Enable keyboard access for all controls\n"
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
 printf "Keyboard - Set a fast keyboard repeat rate\n"
-defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain KeyRepeat -int 2
 
 printf "Keyboard - Set a short pause for repeat\n"
 defaults write NSGlobalDomain InitialKeyRepeat -int 10
